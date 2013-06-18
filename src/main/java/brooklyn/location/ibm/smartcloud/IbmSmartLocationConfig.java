0a5b62cb6190cff492a6a6d33d0b2f3c0bc8e11f
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-06-18 09:28:57 +0100

    use new ConfigKeys syntax

--- src/main/java/brooklyn/location/ibm/smartcloud/IbmSmartLocationConfig.java
+++ src/main/java/brooklyn/location/ibm/smartcloud/IbmSmartLocationConfig.java
@@ -1,29 +1,23 @@
 package brooklyn.location.ibm.smartcloud;
 
 import brooklyn.config.ConfigKey;
-import brooklyn.event.basic.BasicConfigKey;
-import brooklyn.event.basic.BasicConfigKey.StringConfigKey;
+import brooklyn.entity.basic.ConfigKeys;
 import brooklyn.location.cloud.CloudLocationConfig;
 
 public interface IbmSmartLocationConfig extends CloudLocationConfig {
-   public static final ConfigKey<String> LOCATION = new StringConfigKey("location",
+   public static final ConfigKey<String> LOCATION = ConfigKeys.newStringConfigKey("location",
            "Override the location configured (default 'Raleigh')", "Raleigh");
-   public static final ConfigKey<String> IMAGE = new StringConfigKey("image",
+   public static final ConfigKey<String> IMAGE = ConfigKeys.newStringConfigKey("image",
            "Override the image configured (default 'Red Hat Enterprise Linux 6.3 (64-bit)')",
            "Red Hat Enterprise Linux 6.3 (64-bit)");
-   public static final ConfigKey<String> INSTANCE_TYPE_LABEL = new StringConfigKey("instanceType",
+   public static final ConfigKey<String> INSTANCE_TYPE_LABEL = ConfigKeys.newStringConfigKey("instanceType",
            "Override the instanceType configured (default 'Copper')",
            "Copper");
    
-   public static final BasicConfigKey<Integer> MAX_ITERATIONS =
-           new BasicConfigKey<Integer>(Integer.class, "maxIterations", "how many ssh loop attempts (default 120 times)", 120);
+   public static final ConfigKey<Integer> MAX_ITERATIONS =
+           ConfigKeys.newIntegerConfigKey("maxIterations", "how many ssh loop attempts (default 120 times)", 120);
 
+   public static final ConfigKey<Long> PERIOD =
+           ConfigKeys.newLongConfigKey("period", "how long to wait between ssh loop iterations (default 30 seconds)", 30L);
    
-   public static final BasicConfigKey<Long> PERIOD =
-           new BasicConfigKey<Long>(Long.class, "period", "how long to wait between ssh loop iterations (default 30 seconds)", 30L);
-
-   
-//   public static final ConfigKey<Integer> PERIOD = new BasicConfigKey<Integer>(Integer.class,
-//           "period",
-//           , 30);
 }
