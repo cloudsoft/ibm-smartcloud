8d7ed92217967d82bc80219a791eed4d4ee6896a
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-09-03 15:00:44 +0100

    update for tweaks to how commands are invoked

--- src/main/java/brooklyn/location/ibm/smartcloud/IbmSmartCloudSshMachineLocation.java
+++ src/main/java/brooklyn/location/ibm/smartcloud/IbmSmartCloudSshMachineLocation.java
@@ -1,10 +1,12 @@
 package brooklyn.location.ibm.smartcloud;
 
-import brooklyn.location.basic.SshMachineLocation;
-import brooklyn.util.internal.ssh.SshTool;
-import com.google.common.base.Function;
 import java.util.List;
 import java.util.Map;
+
+import brooklyn.location.basic.SshMachineLocation;
+import brooklyn.util.internal.ssh.ShellTool;
+
+import com.google.common.base.Function;
 
 public class IbmSmartCloudSshMachineLocation extends SshMachineLocation {
 
@@ -15,8 +17,8 @@ public class IbmSmartCloudSshMachineLocation extends SshMachineLocation {
    @Override
    public int run(final Map props, final List<String> commands, final Map env) {
       if (commands == null || commands.isEmpty()) return 0;
-      return execSsh(props, new Function<SshTool, Integer>() {
-         public Integer apply(SshTool ssh) {
+      return execSsh(props, new Function<ShellTool, Integer>() {
+         public Integer apply(ShellTool ssh) {
             return ssh.execCommands(props, commands, env);
          }
       });
