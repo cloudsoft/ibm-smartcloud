89a3840599823e8781066dcc8d5d57133cd73cb4
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-09-10 21:43:02 +0100

    cleaner way of setting config when resolving location, not pulling in everything, and setting user etc correctly, and tests

--- src/main/java/brooklyn/location/ibm/smartcloud/IbmSmartCloudResolver.java
+++ src/main/java/brooklyn/location/ibm/smartcloud/IbmSmartCloudResolver.java
@@ -4,6 +4,10 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import brooklyn.config.ConfigUtils;
 import brooklyn.location.Location;
 import brooklyn.location.LocationRegistry;
 import brooklyn.location.LocationResolver;
@@ -11,11 +15,14 @@ import brooklyn.location.LocationSpec;
 import brooklyn.location.basic.BasicLocationRegistry;
 import brooklyn.management.ManagementContext;
 import brooklyn.util.collections.MutableMap;
+import brooklyn.util.config.ConfigBag;
 
 import com.google.common.collect.Maps;
 
 public class IbmSmartCloudResolver implements LocationResolver {
 
+    private static final Logger log = LoggerFactory.getLogger(IbmSmartCloudResolver.class);
+    
    public static final String IBM_SMARTCLOUD = "ibm-smartcloud";
 
    private ManagementContext managementContext;
@@ -40,11 +47,20 @@ public class IbmSmartCloudResolver implements LocationResolver {
 
    @SuppressWarnings("unchecked")
    protected IbmSmartCloudLocation newLocationFromString(String spec, LocationRegistry registry, Map<?,?> properties, Map<?,?> locationFlags) {
-       Map<Object,Object> tmpProperties = Maps.newLinkedHashMap();
+       ConfigBag tmpProperties = ConfigBag.newInstance();
        if (registry!=null) tmpProperties.putAll(registry.getProperties());
        tmpProperties.putAll(properties);
        tmpProperties.putAll(locationFlags);
-       return managementContext.getLocationManager().createLocation(LocationSpec.create(IbmSmartCloudLocation.class).configure(tmpProperties));
+
+       ConfigBag filteredProperties = ConfigBag.newInstance();
+       filteredProperties.putAll(ConfigUtils.filterForPrefixAndStrip(tmpProperties.getAllConfig(), "brooklyn."+IBM_SMARTCLOUD+"."));
+       if (!filteredProperties.isEmpty())
+           log.warn("Properties beginning with deprecated syntax 'brooklyn."+IBM_SMARTCLOUD+"' should be renamed as 'brooklyn.location."+IBM_SMARTCLOUD+"': "+filteredProperties);
+       filteredProperties.putAll(ConfigUtils.filterForPrefixAndStrip(tmpProperties.getAllConfig(), "brooklyn.location."+IBM_SMARTCLOUD+"."));
+       filteredProperties.putAll(locationFlags);
+       
+       return managementContext.getLocationManager().createLocation(LocationSpec.create(IbmSmartCloudLocation.class).configure(
+               filteredProperties.getAllConfig()));
    }
 
    
--- src/test/java/brooklyn/location/ibm/smartcloud/IbmSmartCloudResolverTest.java
+++ src/test/java/brooklyn/location/ibm/smartcloud/IbmSmartCloudResolverTest.java
@@ -4,22 +4,53 @@ import static org.testng.Assert.assertTrue;
 
 import java.util.Map;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import brooklyn.config.BrooklynProperties;
+import brooklyn.config.ConfigUtils;
 import brooklyn.location.Location;
+import brooklyn.location.basic.SshMachineLocation;
 import brooklyn.management.ManagementContext;
 import brooklyn.management.internal.LocalManagementContext;
 import brooklyn.util.collections.MutableMap;
 
 public class IbmSmartCloudResolverTest {
 
-   public static final Map<String, String> PROPS = MutableMap.of("brooklyn.ibm-smartcloud.identity", "x",
-           "brooklyn.ibm-smartcloud.credential", "y", "brooklyn.ibm-smartcloud.user", "idcuser");
+   public static final Map<String, String> FULL_PROPS = MutableMap.of(
+           "brooklyn.location.ibm-smartcloud.identity", "x",
+           "brooklyn.location.ibm-smartcloud.credential", "y", 
+           "brooklyn.location.ibm-smartcloud.user", "idcuser");
+
+   public static final Map<String, Object> PROPS = 
+           ConfigUtils.filterForPrefixAndStrip(FULL_PROPS, "brooklyn.location.ibm-smartcloud.").asMapWithStringKeys();
 
    @Test
-   public void testIbmSmartCloudLoads() {
+   public void testIbmSmartCloudLoadsWithProps() {
       ManagementContext managementContext = new LocalManagementContext();
       Location l = managementContext.getLocationRegistry().resolve("ibm-smartcloud", PROPS);
       assertTrue(l instanceof IbmSmartCloudLocation);
+      Assert.assertEquals(((IbmSmartCloudLocation)l).getUser(), "idcuser");
+      Assert.assertEquals(((IbmSmartCloudLocation)l).getIdentity(), "x");
    }
+   
+   @Test
+   public void testIbmSmartCloudLoadsWithInheritedProps() {
+      BrooklynProperties props = BrooklynProperties.Factory.newEmpty();
+      props.putAll(FULL_PROPS);
+      ManagementContext managementContext = new LocalManagementContext(props);
+      Location l = managementContext.getLocationRegistry().resolve("ibm-smartcloud");
+      assertTrue(l instanceof IbmSmartCloudLocation);
+      Assert.assertEquals(((IbmSmartCloudLocation)l).getUser(), "idcuser");
+      Assert.assertEquals(((IbmSmartCloudLocation)l).getIdentity(), "x");
+   }
+   
+   @Test
+   public void testIbmSmartCloudSshMachineLocationLoads() {
+      ManagementContext managementContext = new LocalManagementContext();
+      IbmSmartCloudLocation l = (IbmSmartCloudLocation) managementContext.getLocationRegistry().resolve("ibm-smartcloud", PROPS);
+      SshMachineLocation ssh = l.createIbmSmartCloudSshMachineLocation("localhost", "id", "/tmp/path");
+      Assert.assertEquals(ssh.getUser(), "idcuser");
+   }
+
 }
